42f680e5fe67c188b47defc21b5d152f8d5d2f1d
sergeyhaki <navent@mail.ru>
2023-03-30 18:57:57 +0300

    Update f2.c

--- f2.c
+++ f2.c
@@ -1,4 +1,4 @@
-ке Си, который использует целочисленную арифметику с 16-битными целыми числами:```c
+/*Си, который использует целочисленную арифметику с 16-битными целыми числами:```c*/
 
 #include <stdio.h>
 
@@ -20,9 +20,11 @@ DFTvoid dft(short *x, short *real, short *imag) {
   for (int k = 0; k < N; k++) {  
   real[k] = imag[k] = 0;  
   for (int n = 0; n < N; n++) {   
-   real[k] += x[n] * cos(2*M_PI*k*n/N);      imag[k] -= x[n] * sin(2*M_PI*k*n/N);   
+   real[k] += x[n] * cos(2*M_PI*k*n/N); 
+     imag[k] -= x[n] * sin(2*M_PI*k*n/N);   
  } 
- }}// Реализация обратного 
+ }
+}// Реализация обратного 
 
 DFTvoid idft(short *real, short *imag, short *x) { 
  for (int n = 0; n < N; n++) {  
@@ -30,4 +32,16 @@ DFTvoid idft(short *real, short *imag, short *x) {
   for (int k = 0; k < N; k++) {  
     x[n] += real[k] * cos(2*M_PI*k*n/N) - imag[k] * sin(2*M_PI*k*n/N);  
   }  
-  x[n] /= N; // Нормировка  }}int main() {  dft(x, real, imag);  idft(real, imag, x);  for (int i = 0; i < N; i++) {    printf("%d\n", x[i]);  }  return 0;}```Здесь `short` обозначает 16-битные целые числа, используемые для представления входных данных, действительной и мнимой частей спектра и 
+  x[n] /= N; // Нормировка  
+}
+}
+
+int main() { 
+ dft(x, real, imag); 
+ idft(real, imag, x); 
+ for (int i = 0; i < N; i++) {    printf("%d\n", x[i]);  } 
+ return 0;
+}
+
+/*```Здесь `short` обозначает 16-битные целые числа, используемые для представления входных данных, действительной и мнимой частей спектра и 
+*/
